f69ec05ae3c1872d5e17b5c2e737a49fb093fb50
Tony <58882912+tuke307@users.noreply.github.com>
2024-06-10 11:53:35 +0900

    Update movieActorRanking.ts

--- app/api/movieActorRanking.ts
+++ app/api/movieActorRanking.ts
@@ -3,9 +3,9 @@ const baseUrl = process.env.MAR_API_URL;
 
 export async function getActors(query: string): Promise<ActorResult[]> {  
     try {
-        console.log(`fetch actors with: ${baseUrl}/search/actor?q=${encodeURIComponent(query)}`);
+        console.log(`fetch actors with: ${baseUrl}/search/classifier/actor?q=${encodeURIComponent(query)}`);
 
-        const response = await fetch(`${baseUrl}/search/actor?q=${encodeURIComponent(query)}`);
+        const response = await fetch(`${baseUrl}/search/classifier/actor?q=${encodeURIComponent(query)}`);
 
         const data = await response.json();
         const actors: ActorResult[] = data as ActorResult[];
